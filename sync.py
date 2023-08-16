0310a2edc6958571f34803cbf0c71f56acdc06f8
myl7 <myl@myl.moe>
2023-08-16 14:07:21 +0800

    Run cmd more carefully and add python code formatting

--- sync.py
+++ sync.py
@@ -1,7 +1,12 @@
 import subprocess
 import json
 import os
-from typing import Dict
+from typing import Dict, List
+
+
+def run_cmd(cmd: List[str]) -> None:
+    subprocess.run(cmd, check=True, text=True)
+
 
 with open('map.json') as f:
     map: Dict[str, str] = json.load(f)
@@ -9,4 +14,4 @@ with open('map.json') as f:
 for dst, src in map.items():
     if src.startswith('~'):
         src = src.replace('~', os.environ.get("HOME"), 1)
-    subprocess.run(['rsync', '-r', src, dst])
+    run_cmd(['rsync', '-r', src, dst])
